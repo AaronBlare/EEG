16a589473bfb8628c8152e0505a59c211a646885
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2021-12-16 10:36:22 +0300

    catboost start

--- classification_test.py
+++ classification_test.py
@@ -1,10 +1,9 @@
 import numpy as np
 import mne
 from mne.externals.pymatreader import read_mat
-import matplotlib.pyplot as plt
-from mne.baseline import rescale
-from mne.stats import bootstrap_confidence_interval
-from sklearn.decomposition import PCA, FastICA
+from mne.stats import permutation_cluster_1samp_test as pcluster_test
+import pandas as pd
+from catboost import CatBoost
 
 data_path = 'E:/YandexDisk/EEG/'
 data_file = 'preproc_data.mat'
@@ -64,12 +63,15 @@ data_right_quasi = fill_data(data_right_quasi, mat_data, 'right_quasi')
 data_right_im1 = fill_data(data_right_im1, mat_data, 'right_im1')
 data_right_im2 = fill_data(data_right_im2, mat_data, 'right_im2')
 
+all_data = np.concatenate((data_right_real, data_right_quasi, data_right_im1, data_right_im2), axis=0)
+
 freq_bands = [('Delta', 0, 4), ('Theta', 4, 8), ('Alpha', 8, 12), ('Beta', 12, 30), ('Gamma', 30, 45)]
 
 
 def get_band_features(data, bands):
-    band_features = np.empty(shape=(data.shape[0], data.shape[1] * 4))
+    band_features = np.empty(shape=(data.shape[0], data.shape[1] * 5 * 4))
     band_features_names = list()
+    band_id = 0
     for band, f_min, f_max in bands:
         filtered_epochs = mne.EpochsArray(data=data.copy(), info=data_info)
         filtered_epochs.filter(f_min, f_max, n_jobs=1, l_trans_bandwidth=1, h_trans_bandwidth=1)
@@ -83,14 +85,15 @@ def get_band_features(data, bands):
             band_features_names.append('_'.join([band, 'max', curr_lead]))
             band_features_names.append('_'.join([band, 'min', curr_lead]))
             for epoch_id in range(0, filtered_data.shape[0]):
-                band_features[epoch_id, 4 * lead_id] = np.mean(filtered_data[epoch_id, lead_id, :])
-                band_features[epoch_id, 4 * lead_id + 1] = np.std(filtered_data[epoch_id, lead_id, :])
-                band_features[epoch_id, 4 * lead_id + 2] = np.max(filtered_data[epoch_id, lead_id, :])
-                band_features[epoch_id, 4 * lead_id + 3] = np.min(filtered_data[epoch_id, lead_id, :])
+                band_features[epoch_id, (band_id + 1) * 4 * lead_id] = np.mean(filtered_data[epoch_id, lead_id, :])
+                band_features[epoch_id, (band_id + 1) * 4 * lead_id + 1] = np.std(filtered_data[epoch_id, lead_id, :])
+                band_features[epoch_id, (band_id + 1) * 4 * lead_id + 2] = np.max(filtered_data[epoch_id, lead_id, :])
+                band_features[epoch_id, (band_id + 1) * 4 * lead_id + 3] = np.min(filtered_data[epoch_id, lead_id, :])
         del filtered_epochs
+        band_id += 1
     return band_features, band_features_names
 
-'''
+
 band_features_right_real, band_features_names_right_real = get_band_features(data_right_real, freq_bands)
 band_features_right_quasi, band_features_names_right_quasi = get_band_features(data_right_quasi, freq_bands)
 band_features_right_im1, band_features_names_right_im1 = get_band_features(data_right_im1, freq_bands)
@@ -120,3 +123,45 @@ def get_spec_features(data, data_info, bands):
 
 spec_features_right_real, spec_features_names_right_real = get_spec_features(data_right_real, data_info, spec_bands)
 spec_features_right_quasi, spec_features_names_right_quasi = get_spec_features(data_right_quasi, data_info, spec_bands)
+'''
+
+all_features = np.concatenate((band_features_right_real, band_features_right_quasi,
+                               band_features_right_im1, band_features_right_im2), axis=0)
+all_names = band_features_names_right_real
+all_classes = [0] * band_features_right_real.shape[0] + [1] * band_features_right_quasi.shape[0] + \
+              [2] * band_features_right_im1.shape[0] + [3] * band_features_right_im2.shape[0]
+
+train_features = np.concatenate((band_features_right_real[:250, :], band_features_right_quasi[:250, :],
+                                 band_features_right_im1[:250, :], band_features_right_im2[:250, :]), axis=0)
+val_features = np.concatenate((band_features_right_real[250:, :], band_features_right_quasi[250:, :],
+                               band_features_right_im1[250:, :], band_features_right_im2[250:, :]), axis=0)
+
+train_classes = [0] * band_features_right_real[:250, :].shape[0] + \
+                [1] * band_features_right_quasi[:250, :].shape[0] + \
+                [2] * band_features_right_im1[:250, :].shape[0] + \
+                [3] * band_features_right_im2[:250, :].shape[0]
+val_classes = [0] * band_features_right_real[250:, :].shape[0] + \
+              [1] * band_features_right_quasi[250:, :].shape[0] + \
+              [2] * band_features_right_im1[250:, :].shape[0] + \
+              [3] * band_features_right_im2[250:, :].shape[0]
+
+model_params = {'classes_count': 4,
+                'loss_function': 'MultiClass',
+                'learning_rate': 0.03,
+                'depth': 6,
+                'min_data_in_leaf': 1,
+                'max_leaves': 31,
+                'verbose': 1,
+                'iterations': 500,
+                'early_stopping_rounds': 100}
+
+model = CatBoost(params=model_params)
+model.fit(train_features, train_classes, eval_set=(val_features, val_classes))
+model.set_feature_names(all_names)
+model.save_model(f"epoch_{model.best_iteration_}.model")
+
+train_pred_probs = model.predict(train_features, prediction_type="Class")
+val_pred_probs = model.predict(val_features, prediction_type="Class")
+
+metrics_summary = {'f1_weighted': 'max', 'auroc_weighted': 'max'}
+metrics = [metrics_classes_dict[m]() for m in metrics_summary]
