8b4d5b703cfced088ff28c6edc98adb149a4dd34
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2022-03-09 23:43:38 +0300

    stat test started

--- create_feature_dataframe_for_file.py
+++ create_feature_dataframe_for_file.py
@@ -6,45 +6,65 @@ from mne.externals.pymatreader import read_mat
 from mne.time_frequency import psd_welch
 
 data_path = 'E:/YandexDisk/EEG/Data/'
-data_files = ['1st_Day.mat']
+data_files = ['2nd_Day_TMS.mat', '2nd_Day_TMS_5new_subj.mat']
 
 dataframe_path = 'E:/YandexDisk/EEG/Dataframes/'
+
+background = True
 
 mat_data = []
 for file_id in range(0, len(data_files)):
     curr_mat_data = read_mat(data_path + data_files[file_id])
     mat_data.append(curr_mat_data)
     if file_id == 0:
-        if 'subs_ica' in curr_mat_data:
-            data_info = mne.create_info(ch_names=curr_mat_data['subs_ica'][0]['right_real']['label'],
-                                        sfreq=curr_mat_data['subs_ica'][0]['right_real']['fsample'],
-                                        ch_types='eeg')
-        elif 'subs_ica_bgr' in curr_mat_data:
-            data_info = mne.create_info(ch_names=curr_mat_data['subs_ica_bgr'][0]['right_real']['label'],
-                                        sfreq=curr_mat_data['subs_ica_bgr'][0]['right_real']['fsample'],
-                                        ch_types='eeg')
+        if background:
+            if 'subs_ica' in curr_mat_data:
+                data_info = mne.create_info(ch_names=curr_mat_data['subs_ica'][0]['right_real']['label'],
+                                            sfreq=curr_mat_data['subs_ica'][0]['right_real']['fsample'],
+                                            ch_types='eeg')
+            else:
+                data_info = mne.create_info(ch_names=curr_mat_data['res'][0]['right_real']['label'],
+                                            sfreq=curr_mat_data['res'][0]['right_real']['fsample'],
+                                            ch_types='eeg')
         else:
-            data_info = mne.create_info(ch_names=curr_mat_data['res'][0]['right_real']['label'],
-                                        sfreq=curr_mat_data['res'][0]['right_real']['fsample'],
-                                        ch_types='eeg')
+            if 'subs_ica_bgr' in curr_mat_data:
+                data_info = mne.create_info(ch_names=curr_mat_data['subs_ica_bgr'][0]['right_real']['label'],
+                                            sfreq=curr_mat_data['subs_ica_bgr'][0]['right_real']['fsample'],
+                                            ch_types='eeg')
+            else:
+                data_info = mne.create_info(ch_names=curr_mat_data['res_bgr'][0]['right_real']['label'],
+                                            sfreq=curr_mat_data['res_bgr'][0]['right_real']['fsample'],
+                                            ch_types='eeg')
     del curr_mat_data
 
 
 def calculate_num_trials(data, move_type):
     num_trials = 0
-    if 'subs_ica' in data:
-        file_flag = 'subs_ica'
-    elif 'subs_ica_bgr' in data:
-        file_flag = 'subs_ica_bgr'
+    if background:
+        if 'subs_ica_bgr' in data:
+            file_flag = 'subs_ica_bgr'
+        else:
+            file_flag = 'res_bgr'
+        min_trial_len = data[file_flag][0][move_type]['trial'].shape[1]
     else:
-        file_flag = 'res'
-    min_trial_len = data[file_flag][0][move_type]['trial'][0].shape[1]
+        if 'subs_ica' in data:
+            file_flag = 'subs_ica'
+        else:
+            file_flag = 'res'
+        min_trial_len = data[file_flag][0][move_type]['trial'][0].shape[1]
     for subject_id in range(0, len(data[file_flag])):
         if len(data[file_flag][subject_id]) > 0:
-            num_trials += len(data[file_flag][subject_id][move_type]['trial'])
+            if background:
+                num_trials += 1
+            else:
+                num_trials += len(data[file_flag][subject_id][move_type]['trial'])
             for trial_id in range(0, len(data[file_flag][subject_id][move_type]['trial'])):
-                if data[file_flag][subject_id][move_type]['trial'][trial_id].shape[1] < min_trial_len:
-                    min_trial_len = data[file_flag][subject_id][move_type]['trial'][trial_id].shape[1]
+                if background:
+                    if data[file_flag][subject_id][move_type]['trial'].shape[1] < min_trial_len:
+                        min_trial_len = data[file_flag][subject_id][move_type]['trial'][trial_id].shape[1]
+                else:
+                    if data[file_flag][subject_id][move_type]['trial'][trial_id].shape[1] < min_trial_len:
+                        min_trial_len = data[file_flag][subject_id][move_type]['trial'].shape[1]
     return num_trials, min_trial_len
 
 
@@ -58,13 +78,18 @@ for movement_type in movement_types:
     for file_id in range(0, len(data_files)):
         curr_data = mat_data[file_id]
         curr_num_trials, curr_min_trial_len = calculate_num_trials(curr_data, movement_type)
-        if 'subs_ica' in curr_data:
-            file_flag = 'subs_ica'
-        elif 'subs_ica_bgr' in curr_data:
-            file_flag = 'subs_ica_bgr'
+        if background:
+            if 'subs_ica_bgr' in curr_data:
+                file_flag = 'subs_ica_bgr'
+            else:
+                file_flag = 'res_bgr'
+            curr_num_electrodes = curr_data[file_flag][0][movement_type]['trial'].shape[0]
         else:
-            file_flag = 'res'
-        curr_num_electrodes = curr_data[file_flag][0][movement_type]['trial'][0].shape[0]
+            if 'subs_ica' in curr_data:
+                file_flag = 'subs_ica'
+            else:
+                file_flag = 'res'
+            curr_num_electrodes = curr_data[file_flag][0][movement_type]['trial'][0].shape[0]
         min_trial_len_by_movement.append(curr_min_trial_len)
         num_electrodes_by_movement.append(curr_num_electrodes)
         movement_num_trials += curr_num_trials
@@ -73,33 +98,51 @@ for movement_type in movement_types:
 
 min_trial_len = min(min_trial_len_by_movement)
 num_electrodes = min(num_electrodes_by_movement)
+trial_start = min_trial_len // 2
 
-data = {movement_type: np.empty(shape=(num_trials_by_movement[movement_type], num_electrodes, min_trial_len)) for
-        movement_type in movement_types}
+data = {
+    movement_type: np.empty(shape=(num_trials_by_movement[movement_type], num_electrodes, min_trial_len - trial_start))
+    for movement_type in movement_types}
 
 
-def fill_data(data, raw_data, movement_types, trial_len):
+def fill_data(data, raw_data, movement_types, trial_start, trial_len):
+    subjects = []
     for movement_type in movement_types:
         curr_trial = 0
+        curr_subject = 0
         for file_id in range(0, len(data_files)):
             curr_file = raw_data[file_id]
-            if 'subs_ica' in curr_file:
-                file_flag = 'subs_ica'
-            elif 'subs_ica_bgr' in curr_file:
-                file_flag = 'subs_ica_bgr'
-            else:
-                file_flag = 'res'
-            for subject_id in range(0, len(curr_file[file_flag])):
-                if len(curr_file[file_flag][subject_id]) > 0:
-                    for trial_id in range(0, len(curr_file[file_flag][subject_id][movement_type]['trial'])):
+            if background:
+                if 'subs_ica_bgr' in curr_file:
+                    file_flag = 'subs_ica_bgr'
+                else:
+                    file_flag = 'res_bgr'
+                for subject_id in range(0, len(curr_file[file_flag])):
+                    if len(curr_file[file_flag][subject_id]) > 0:
+                        subjects.append(f'S{curr_subject}_T{0}_{movement_type}')
                         data[movement_type][curr_trial, :, :] = \
-                            curr_file[file_flag][subject_id][movement_type]['trial'][trial_id][:, :trial_len]
+                            curr_file[file_flag][subject_id][movement_type]['trial'][:, trial_start:trial_len]
                         curr_trial += 1
+                    curr_subject += 1
+            else:
+                if 'subs_ica' in curr_file:
+                    file_flag = 'subs_ica'
+                else:
+                    file_flag = 'res'
+                for subject_id in range(0, len(curr_file[file_flag])):
+                    if len(curr_file[file_flag][subject_id]) > 0:
+                        for trial_id in range(0, len(curr_file[file_flag][subject_id][movement_type]['trial'])):
+                            subjects.append(f'S{curr_subject}_T{trial_id}_{movement_type}')
+                            data[movement_type][curr_trial, :, :] = \
+                                curr_file[file_flag][subject_id][movement_type]['trial'][trial_id][:,
+                                trial_start:trial_len]
+                            curr_trial += 1
+                        curr_subject += 1
             del curr_file
-    return data
+    return data, subjects
 
 
-data = fill_data(data, mat_data, movement_types, min_trial_len)
+data, subjects = fill_data(data, mat_data, movement_types, trial_start, min_trial_len)
 
 freq_bands = [('Theta', 4, 8), ('Alpha', 8, 12), ('Beta', 12, 30), ('Gamma', 30, 45)]
 
@@ -115,15 +158,15 @@ def get_band_features(data, movement_types, bands):
         filtered_epochs = mne.EpochsArray(data=data[movement_type].copy(), info=data_info)
         filtered_epochs.filter(1, 50, n_jobs=1, l_trans_bandwidth=1, h_trans_bandwidth=1)
         psd, freqs = psd_welch(filtered_epochs, fmin=1, fmax=45, tmin=0, tmax=data[movement_types[0]].shape[2])
-        band_freqs_ids = [[]]
+        psd_band_freqs_ids = [[]]
         curr_band_id = 0
         for freq_id in range(0, freqs.shape[0]):
             freq = freqs[freq_id]
             if freq > bands[curr_band_id][2]:
-                band_freqs_ids.append([])
+                psd_band_freqs_ids.append([])
                 curr_band_id += 1
             if bands[curr_band_id][1] <= freq <= bands[curr_band_id][2]:
-                band_freqs_ids[curr_band_id].append(freq_id)
+                psd_band_freqs_ids[curr_band_id].append(freq_id)
         feature_id = 0
         for band_id in range(0, len(bands)):
             band = bands[band_id][0]
@@ -152,7 +195,7 @@ def get_band_features(data, movement_types, bands):
                     band_features[movement_type][trial_id, feature_id + 4] = np.min(
                         filtered_data[trial_id, electrode_id, :])
                     band_features[movement_type][trial_id, feature_id + 5] = np.mean(
-                        psd[trial_id, electrode_id, band_freqs_ids[band_id]])
+                        psd[trial_id, electrode_id, psd_band_freqs_ids[band_id]])
                 feature_id += num_features_types
         del filtered_epochs
     return band_features, band_features_names
@@ -161,6 +204,7 @@ def get_band_features(data, movement_types, bands):
 band_features, band_features_names = get_band_features(data, movement_types, freq_bands)
 
 features = np.concatenate([band_features[movement_type] for movement_type in movement_types], axis=0)
+subjects_features = np.concatenate((np.c_[subjects], features), axis=1)
 
 features_names = band_features_names
 
@@ -171,8 +215,13 @@ for movement_type_id in range(0, len(movement_types)):
     classes.extend([movement_type_id] * band_features[movement_type].shape[0])
     classes_names.extend([movement_type] * band_features[movement_type].shape[0])
 
-df = pd.DataFrame(np.concatenate((features, np.c_[classes_names]), axis=1), columns=features_names + ['class'])
+df = pd.DataFrame(np.concatenate((subjects_features, np.c_[classes_names]), axis=1),
+                  columns=['trial'] + features_names + ['class'])
 
 Path(dataframe_path).mkdir(parents=True, exist_ok=True)
 data_files_wo_extension = [data_file[:-4] for data_file in data_files]
-df.to_excel(f"{dataframe_path}/dataframe_{'_'.join(data_files_wo_extension)}.xlsx", header=True, index=False)
+if background:
+    df.to_excel(f"{dataframe_path}/dataframe_{'_'.join(data_files_wo_extension)}_background.xlsx",
+                header=True, index=False)
+else:
+    df.to_excel(f"{dataframe_path}/dataframe_{'_'.join(data_files_wo_extension)}.xlsx", header=True, index=False)
---
+++ stat_test_distribution.py
@@ -0,0 +1,116 @@
+import numpy as np
+import mne
+from mne.externals.pymatreader import read_mat
+import pandas as pd
+import scipy
+from scipy.stats import kruskal, mannwhitneyu
+import statsmodels
+from statsmodels.stats.multitest import multipletests
+import os
+
+
+data_path = 'E:/YandexDisk/EEG/Data/'
+data_files = ['2nd_Day_TMS.mat', '2nd_Day_TMS_5new_subj.mat']
+
+dataframe_path = 'E:/YandexDisk/EEG/Dataframes/'
+data_files_wo_extension = [data_file[:-4] for data_file in data_files]
+xlsx_file = f"{dataframe_path}/dataframe_{'_'.join(data_files_wo_extension)}.xlsx"
+
+dataframe_stat_path = 'E:/YandexDisk/EEG/Stat/'
+
+if os.path.isfile(xlsx_file):
+    df = pd.read_excel(xlsx_file)
+    df_im1 = df.loc[df['class'] == 'right_im1']
+    df_im2 = df.loc[df['class'] == 'right_im2']
+    mannwhitney_TMS_im1_im2 = {'feature': [], 'pval': [], 'pval_bh': []}
+    for column in df:
+        if column != 'class' and column != 'trial':
+            curr_im1 = df_im1[column]
+            curr_im2 = df_im2[column]
+            res = mannwhitneyu(curr_im1, curr_im2)
+            pval = res.pvalue
+            if not np.isnan(pval):
+                mannwhitney_TMS_im1_im2['pval'].append(pval)
+                mannwhitney_TMS_im1_im2['feature'].append(column)
+    reject, pval_bh, alphacSidak, alphacBonf = multipletests(mannwhitney_TMS_im1_im2['pval'], method='fdr_bh')
+    mannwhitney_TMS_im1_im2['pval_bh'] = pval_bh
+    df = pd.DataFrame.from_dict(mannwhitney_TMS_im1_im2)
+    df.to_excel(f"{dataframe_stat_path}/mannwhitney_TMS_im1_im2.xlsx", header=True, index=False)
+
+
+data_files = ['DAY2_SHAM.mat']
+
+data_files_wo_extension = [data_file[:-4] for data_file in data_files]
+xlsx_file = f"{dataframe_path}/dataframe_{'_'.join(data_files_wo_extension)}.xlsx"
+
+if os.path.isfile(xlsx_file):
+    df = pd.read_excel(xlsx_file)
+    df_im1 = df.loc[df['class'] == 'right_im1']
+    df_im2 = df.loc[df['class'] == 'right_im2']
+    mannwhitney_sham_im1_im2 = {'feature': [], 'pval': [], 'pval_bh': []}
+    for column in df:
+        if column != 'class' and column != 'trial':
+            curr_im1 = df_im1[column]
+            curr_im2 = df_im2[column]
+            res = mannwhitneyu(curr_im1, curr_im2)
+            pval = res.pvalue
+            if not np.isnan(pval):
+                mannwhitney_sham_im1_im2['pval'].append(pval)
+                mannwhitney_sham_im1_im2['feature'].append(column)
+    reject, pval_bh, alphacSidak, alphacBonf = multipletests(mannwhitney_sham_im1_im2['pval'], method='fdr_bh')
+    mannwhitney_sham_im1_im2['pval_bh'] = pval_bh
+    df = pd.DataFrame.from_dict(mannwhitney_sham_im1_im2)
+    df.to_excel(f"{dataframe_stat_path}/mannwhitney_sham_im1_im2.xlsx", header=True, index=False)
+
+
+data_files = ['1st_Day.mat']
+
+data_files_wo_extension = [data_file[:-4] for data_file in data_files]
+xlsx_file = f"{dataframe_path}/dataframe_{'_'.join(data_files_wo_extension)}.xlsx"
+
+if os.path.isfile(xlsx_file):
+    df = pd.read_excel(xlsx_file)
+    df_im1 = df.loc[df['class'] == 'right_im1']
+    df_im2 = df.loc[df['class'] == 'right_im2']
+    mannwhitney_1st_day_im1_im2 = {'feature': [], 'pval': [], 'pval_bh': []}
+    for column in df:
+        if column != 'class' and column != 'trial':
+            curr_im1 = df_im1[column]
+            curr_im2 = df_im2[column]
+            res = mannwhitneyu(curr_im1, curr_im2)
+            pval = res.pvalue
+            if not np.isnan(pval):
+                mannwhitney_1st_day_im1_im2['pval'].append(pval)
+                mannwhitney_1st_day_im1_im2['feature'].append(column)
+    reject, pval_bh, alphacSidak, alphacBonf = multipletests(mannwhitney_1st_day_im1_im2['pval'], method='fdr_bh')
+    mannwhitney_1st_day_im1_im2['pval_bh'] = pval_bh
+    df = pd.DataFrame.from_dict(mannwhitney_1st_day_im1_im2)
+    df.to_excel(f"{dataframe_stat_path}/mannwhitney_1st_day_im1_im2.xlsx", header=True, index=False)
+
+
+data_files = ['2nd_Day_TMS.mat', '2nd_Day_TMS_5new_subj.mat']
+
+data_files_wo_extension = [data_file[:-4] for data_file in data_files]
+xlsx_file = f"{dataframe_path}/dataframe_{'_'.join(data_files_wo_extension)}_background.xlsx"
+
+if os.path.isfile(xlsx_file):
+    df = pd.read_excel(xlsx_file)
+    df_real = df.loc[df['class'] == 'right_real']
+    df_quasi = df.loc[df['class'] == 'right_quasi']
+    df_im1 = df.loc[df['class'] == 'right_im1']
+    df_im2 = df.loc[df['class'] == 'right_im2']
+    kruskal_TMS_real_quasi_im1_im2 = {'feature': [], 'pval': [], 'pval_bh': []}
+    for column in df:
+        if column != 'class' and column != 'trial':
+            curr_real = df_real[column]
+            curr_quasi = df_quasi[column]
+            curr_im1 = df_im1[column]
+            curr_im2 = df_im2[column]
+            stat, pval = kruskal(curr_real, curr_im1, curr_im1, curr_im2)
+            if not np.isnan(pval):
+                kruskal_TMS_real_quasi_im1_im2['pval'].append(pval)
+                kruskal_TMS_real_quasi_im1_im2['feature'].append(column)
+    reject, pval_bh, alphacSidak, alphacBonf = multipletests(kruskal_TMS_real_quasi_im1_im2['pval'], method='fdr_bh')
+    kruskal_TMS_real_quasi_im1_im2['pval_bh'] = pval_bh
+    df = pd.DataFrame.from_dict(kruskal_TMS_real_quasi_im1_im2)
+    df.to_excel(f"{dataframe_stat_path}/kruskal_TMS_real_quasi_im1_im2.xlsx", header=True, index=False)
