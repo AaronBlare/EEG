733babad89d5b4b2eca36d74e1f6bed56e1daa9c
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2022-03-20 01:59:37 +0300

    added colormaps

--- calculate_erds.py
+++ calculate_erds.py
@@ -3,9 +3,13 @@ import numpy as np
 from pathlib import Path
 import mne
 from mne.externals.pymatreader import read_mat
+import plotly.graph_objects as go
 
 data_path = 'E:/YandexDisk/EEG/Data/'
 data_files = ['DAY2_SHAM.mat']
+
+figures_path = 'E:/YandexDisk/EEG/Figures/ERDS/'
+suffix = '2nd_Day_sham'
 
 movement_types = ['right_real', 'right_quasi', 'right_im1', 'right_im2']
 
@@ -90,6 +94,21 @@ rest_data_averaged_over_trials_1D = {move_type: np.mean(rest_data_1D[move_type],
 ERDS_1D = {move_type: (action_data_averaged_over_trials_1D[move_type] - rest_data_averaged_over_trials_1D[move_type]) /
                       rest_data_averaged_over_trials_1D[move_type] for move_type in movement_types}
 
+for electrode_id in range(0, num_electrodes):
+    for movement_type in movement_types:
+        Path(f"{figures_path}/{suffix}/{movement_type}/1D/").mkdir(parents=True, exist_ok=True)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=np.linspace(0, min_trial_len / 1000, min_trial_len),
+                                 y=ERDS_1D[movement_type][electrode_id],
+                                 mode='lines'))
+        fig.add_annotation(dict(font=dict(color="black", size=14), x=0.5, y=-0.1, showarrow=False,
+                                text="Time, s", xref="paper", yref="paper"))
+        fig.add_annotation(dict(font=dict(color="black", size=14), x=-0.33, y=0.5, showarrow=False,
+                                text="ERDS", textangle=-90, xref="paper", yref="paper"))
+        fig.update_layout(margin=dict(t=50, l=200))
+        fig.write_image(f"{figures_path}/{suffix}/{movement_type}/1D/{data_info['ch_names'][electrode_id]}.png")
+        fig.write_image(f"{figures_path}/{suffix}/{movement_type}/1D/{data_info['ch_names'][electrode_id]}.pdf")
+
 del action_data_V, rest_data_V, action_data_1D, rest_data_1D
 
 ERDS_2D = {move_type: np.empty(shape=(num_freqs, num_electrodes, min_trial_len)) for move_type in movement_types}
@@ -99,6 +118,7 @@ for electrode_id in range(0, num_electrodes):
         freq_start = freqs[0] + freq_id
         freq_finish = freqs[0] + freq_id + 1
         for movement_type in movement_types:
+            Path(f"{figures_path}/{suffix}/{movement_type}/2D/").mkdir(parents=True, exist_ok=True)
             curr_electrode_freq_action = mne.filter.filter_data(action_data_uV[movement_type][:, electrode_id, :],
                                                                 data_info['sfreq'],
                                                                 freq_start, freq_finish)
@@ -112,5 +132,17 @@ for electrode_id in range(0, num_electrodes):
             curr_electrode_freq_action_averaged = np.mean(curr_electrode_freq_action, axis=0)
             curr_electrode_freq_rest_averaged = np.mean(curr_electrode_freq_rest, axis=0)
             ERDS_2D[movement_type][freq_id, electrode_id, :] = (curr_electrode_freq_action_averaged - curr_electrode_freq_rest_averaged) / curr_electrode_freq_rest_averaged
+
+    for movement_type in movement_types:
+        fig = go.Figure(data=go.Heatmap(x=np.linspace(0, min_trial_len / 1000, min_trial_len),
+                                        y=np.linspace(freqs[0], freqs[1], num_freqs),
+                                        z=ERDS_2D[movement_type][:, electrode_id, :]))
+        fig.add_annotation(dict(font=dict(color="black", size=14), x=0.5, y=-0.1, showarrow=False,
+                                text="Time, s", xref="paper", yref="paper"))
+        fig.add_annotation(dict(font=dict(color="black", size=14), x=-0.33, y=0.5, showarrow=False,
+                                text="Frequency, Hz", textangle=-90, xref="paper", yref="paper"))
+        fig.update_layout(margin=dict(t=50, l=200))
+        fig.write_image(f"{figures_path}/{suffix}/{movement_type}/2D/{data_info['ch_names'][electrode_id]}.png")
+        fig.write_image(f"{figures_path}/{suffix}/{movement_type}/2D/{data_info['ch_names'][electrode_id]}.pdf")
 
 olo = 0
