02d73ce296cd9b031d70a604fa5f6e58285da934
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2022-06-27 21:15:56 +0300

    plot legend from Archie

--- scripts/dimensionality_reduction.py
+++ scripts/dimensionality_reduction.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA, KernelPCA, SparsePCA
 import plotly.graph_objects as go
 import plotly.express as px
-from scripts.plot_functions import add_scatter_trace, add_layout
+from scripts.plot_functions import plot_scatter_by_subject, plot_scatter
 
 
 experiment_name = '1st_day'
@@ -28,6 +28,9 @@ classes = data_df.loc[:, ['class_simp']].values
 # data = StandardScaler().fit_transform(data)
 # data = pd.DataFrame(data)
 
+colors = px.colors.sample_colorscale("turbo", [col / (num_subjects - 1) for col in range(num_subjects)])
+marker_symbols = ['circle', 'x']
+
 # PCA ==================================================================================================================
 
 save_path = f'E:/YandexDisk/EEG/Figures/dimensionality_reduction/{experiment_name}/PCA/'
@@ -41,38 +44,14 @@ data_pca['class'] = classes
 pca_columns = ["PC1", "PC2", 'subject', 'class']
 data_pca.columns = pca_columns
 
-marker_symbols = ['circle', 'x']
 for experiment in experiment_types:
     fig = go.Figure()
-    for movement_id in range(0, len(experiment)):
-        movement = experiment[movement_id]
-        symbol = marker_symbols[movement_id]
-        curr_movement_data = data_pca.loc[data_pca['class'] == movement]
-        for subject_id in range(0, num_subjects):
-            curr_subject_data = curr_movement_data.loc[curr_movement_data['subject'] == f'S{subject_id}']
-            add_scatter_trace(fig,
-                              curr_subject_data['PC1'].values,
-                              curr_subject_data['PC2'].values,
-                              f'S{subject_id}_{movement}',
-                              symbol)
-    add_layout(fig, f'PC1', f'PC2', f'')
-    colors = px.colors.sample_colorscale("turbo", [col / (num_subjects - 1) for col in range(num_subjects)])
-    colors.extend(colors)
-    fig.update_layout({'colorway': colors})
-    fig.update_layout(legend_font_size=13)
+    plot_scatter_by_subject(fig, experiment, marker_symbols, colors, data_pca, num_subjects, 'PC1', 'PC2', 'PCA')
     fig.write_image(f"{save_path}subject_{'_'.join(experiment)}.png")
     fig.write_image(f"{save_path}subject_{'_'.join(experiment)}.pdf", format="pdf")
 
     fig = go.Figure()
-    for movement_id in range(0, len(experiment)):
-        movement = experiment[movement_id]
-        symbol = marker_symbols[0]
-        curr_movement_data = data_pca.loc[data_pca['class'] == movement]
-        add_scatter_trace(fig, curr_movement_data['PC1'].values, curr_movement_data['PC2'].values, movement, symbol)
-    add_layout(fig, f'PC1', f'PC2', f'')
-    fig.update_layout({'colorway': px.colors.qualitative.D3})
-    fig.update_layout(legend_font_size=20)
-    fig.update_layout(margin=go.layout.Margin(l=90, r=20, b=80, t=65, pad=0))
+    plot_scatter(fig, experiment, marker_symbols, data_pca, 'PC1', 'PC2', 'PCA')
     fig.write_image(f"{save_path}{'_'.join(experiment)}.png")
     fig.write_image(f"{save_path}{'_'.join(experiment)}.pdf", format="pdf")
 
@@ -89,38 +68,14 @@ data_kpca['class'] = classes
 kpca_columns = ["PC1", "PC2", 'subject', 'class']
 data_kpca.columns = kpca_columns
 
-marker_symbols = ['circle', 'x']
 for experiment in experiment_types:
     fig = go.Figure()
-    for movement_id in range(0, len(experiment)):
-        movement = experiment[movement_id]
-        symbol = marker_symbols[movement_id]
-        curr_movement_data = data_kpca.loc[data_kpca['class'] == movement]
-        for subject_id in range(0, num_subjects):
-            curr_subject_data = curr_movement_data.loc[curr_movement_data['subject'] == f'S{subject_id}']
-            add_scatter_trace(fig,
-                              curr_subject_data['PC1'].values,
-                              curr_subject_data['PC2'].values,
-                              f'S{subject_id}_{movement}',
-                              symbol)
-    add_layout(fig, f'PC1', f'PC2', f'')
-    colors = px.colors.sample_colorscale("turbo", [col / (num_subjects - 1) for col in range(num_subjects)])
-    colors.extend(colors)
-    fig.update_layout({'colorway': colors})
-    fig.update_layout(legend_font_size=13)
+    plot_scatter_by_subject(fig, experiment, marker_symbols, colors, data_kpca, num_subjects, 'PC1', 'PC2', 'KernelPCA')
     fig.write_image(f"{save_path}subject_{'_'.join(experiment)}.png")
     fig.write_image(f"{save_path}subject_{'_'.join(experiment)}.pdf", format="pdf")
 
     fig = go.Figure()
-    for movement_id in range(0, len(experiment)):
-        movement = experiment[movement_id]
-        symbol = marker_symbols[0]
-        curr_movement_data = data_kpca.loc[data_kpca['class'] == movement]
-        add_scatter_trace(fig, curr_movement_data['PC1'].values, curr_movement_data['PC2'].values, movement, symbol)
-    add_layout(fig, f'PC1', f'PC2', f'')
-    fig.update_layout({'colorway': px.colors.qualitative.D3})
-    fig.update_layout(legend_font_size=20)
-    fig.update_layout(margin=go.layout.Margin(l=90, r=20, b=80, t=65, pad=0))
+    plot_scatter(fig, experiment, marker_symbols, data_kpca, 'PC1', 'PC2', 'KernelPCA')
     fig.write_image(f"{save_path}{'_'.join(experiment)}.png")
     fig.write_image(f"{save_path}{'_'.join(experiment)}.pdf", format="pdf")
 
--- scripts/plot_functions.py
+++ scripts/plot_functions.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+import plotly.express as px
 
 
 def add_scatter_trace(fig, x, y, name, marker_symbol, mode='markers', size=8):
@@ -15,9 +16,35 @@ def add_scatter_trace(fig, x, y, name, marker_symbol, mode='markers', size=8):
                 opacity=0.9,
                 symbol=marker_symbol,
                 line=dict(
+                    color='black',
                     width=0.05
                 )
             )
+        )
+    )
+
+
+def add_scatter_trace_subj(fig, x, y, name, subj, color, marker_symbol, mode='markers', size=8):
+    showlegend = False if name == "" else True
+    fig.add_trace(
+        go.Scatter(
+            x=x,
+            y=y,
+            showlegend=showlegend,
+            name=name,
+            mode=mode,
+            marker=dict(
+                color=color,
+                size=size,
+                opacity=0.9,
+                symbol=marker_symbol,
+                line=dict(
+                    color='black',
+                    width=0.05
+                )
+            ),
+            legendgroup=f"{subj}",
+            legendgrouptitle_text=f"{subj}",
         )
     )
 
@@ -37,7 +64,8 @@ def add_layout(fig, x_label, y_label, title, font_size=25):
             text=title,
             font=dict(
                 size=font_size
-            )
+            ),
+            y=0.99
         ),
         autosize=True,
         showlegend=True,
@@ -72,3 +100,69 @@ def get_axis(title, title_size, tick_size):
         showexponent='all'
     )
     return axis
+
+
+def legend_layout(fig, x_label, y_label, title):
+    fig.update_layout(
+        template="none",
+        legend=dict(
+            itemsizing='constant',
+            orientation="h",
+            font_size=20,
+        ),
+        margin=go.layout.Margin(
+            l=120,
+            r=20,
+            b=120,
+            t=230,
+            pad=0
+        ),
+        title=dict(
+            text=title,
+            xref="paper",
+            font=dict(
+                size=45
+            ),
+            y=0.99
+        ),
+        showlegend=True,
+        xaxis=get_axis(x_label, 30, 30),
+        yaxis=get_axis(y_label, 30, 30),
+        autosize=False,
+        width=900,
+        height=900,
+    )
+
+
+def plot_scatter_by_subject(fig, experiment, marker_symbols, colors, data, num_subjects, x_axis, y_axis, title):
+    for movement_id in range(0, len(experiment)):
+        movement = experiment[movement_id]
+        symbol = marker_symbols[movement_id]
+        curr_movement_data = data.loc[data['class'] == movement]
+        for subject_id in range(0, num_subjects):
+            curr_subject_data = curr_movement_data.loc[curr_movement_data['subject'] == f'S{subject_id}']
+            add_scatter_trace_subj(fig,
+                                   curr_subject_data[x_axis].values,
+                                   curr_subject_data[y_axis].values,
+                                   movement.split('_')[0],
+                                   f'S{subject_id}',
+                                   colors[subject_id],
+                                   symbol)
+    add_layout(fig, x_axis, y_axis, f'')
+    legend_layout(fig, x_axis, y_axis, title)
+
+
+def plot_scatter(fig, experiment, marker_symbols, data, x_axis, y_axis, title):
+    for movement_id in range(0, len(experiment)):
+        movement = experiment[movement_id]
+        symbol = marker_symbols[0]
+        curr_movement_data = data.loc[data['class'] == movement]
+        add_scatter_trace(fig,
+                          curr_movement_data[x_axis].values,
+                          curr_movement_data[y_axis].values,
+                          movement.split('_')[0],
+                          symbol)
+    add_layout(fig, x_axis, y_axis, title)
+    fig.update_layout({'colorway': px.colors.qualitative.D3})
+    fig.update_layout(legend_font_size=20)
+    fig.update_layout(margin=go.layout.Margin(l=90, r=20, b=80, t=65, pad=0))
