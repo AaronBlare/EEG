4d116c4f0a71d474cd93f4b0978e2080b7353ec9
Aaron Blare <alena.kalyakulina@itmm.unn.ru>
2022-06-30 15:20:16 +0300

    pkl not pickle

--- scripts/convert_xlsx_to_pkl.py
+++ scripts/convert_xlsx_to_pkl.py
@@ -6,5 +6,5 @@ experiments = ['1st_day', '2nd_day_sham', '2nd_day_tms']
 for experiment in experiments:
     data_path = f'E:/YandexDisk/EEG/experiments/{experiment}/'
     data_df = pd.read_excel(f'{data_path}data.xlsx')
-    with open(f'{data_path}data.pickle', 'wb') as handle:
+    with open(f'{data_path}data.pkl', 'wb') as handle:
         pickle.dump(data_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
